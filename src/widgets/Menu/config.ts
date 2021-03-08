717b278d9a9769667ef484866fd2bc8d61231a04
deflate-finance <deflatefi@protonmail.com>
2021-03-07 17:51:29 -0800

    initial commit

---
+++ src/widgets/Menu/config.ts
@@ -0,0 +1,93 @@
+export const links = [
+  {
+    label: "Home",
+    icon: "HomeIcon",
+    href: "/",
+  },
+  {
+    label: "Trade",
+    icon: "TradeIcon",
+    items: [
+      {
+        label: 'Buy Balloon',
+        href: 'https://exchange.pancakeswap.finance/#/swap?outputCurrency=0x428a38a7bdca24cd22623bd427efc64077b56cc0',
+      },
+      {
+        label: 'Add Balloon Liquidity',
+        href: 'https://exchange.pancakeswap.finance/#/add/ETH/0x428a38a7BdCa24CD22623bd427efC64077b56cC0',
+      },
+    ],
+  },
+  {
+    label: "Farms",
+    icon: "FarmIcon",
+    href: "/farms",
+  },
+  {
+    label: "Info",
+    icon: "InfoIcon",
+    items: [
+      {
+        label: "Overview",
+        href: "https://pancakeswap.info",
+      },
+      {
+        label: "Tokens",
+        href: "https://pancakeswap.info/tokens",
+      },
+      {
+        label: "Pairs",
+        href: "https://pancakeswap.info/pairs",
+      },
+      {
+        label: "Accounts",
+        href: "https://pancakeswap.info/accounts",
+      },
+    ],
+  },
+  {
+    label: "More",
+    icon: "MoreIcon",
+    items: [
+      {
+        label: "Github",
+        href: "https://github.com/deflate-finance",
+      },
+      {
+        label: "Docs",
+        href: "https://deflatefi.gitbook.io/",
+      },
+      {
+        label: "Blog",
+        href: "https://medium.com/@deflatefi",
+      },
+    ],
+  },
+];
+
+export const socials = [
+  {
+    label: "Telegram",
+    icon: "TelegramIcon",
+    items: [
+      {
+          label: "English",
+          href: "http://t.me/deflatechat",
+      },
+      {
+          label: "Announcements",
+          href: "https://t.me/deflateann",
+      },
+    ],
+  },
+  {
+    label: "Twitter",
+    icon: "TwitterIcon",
+    href: "https://twitter.com/DeflateFinance",
+  },
+];
+
+export const MENU_HEIGHT = 64;
+export const MENU_ENTRY_HEIGHT = 48;
+export const SIDEBAR_WIDTH_FULL = 240;
+export const SIDEBAR_WIDTH_REDUCED = 56;
